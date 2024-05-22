16fbc861b948d28b8618a4562cf5fe0070c96333
strzelus11 <103840690+strzelus11@users.noreply.github.com>
2024-05-22 15:50:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className='sticky top-0 z-10'>
+          <Header />
+    </div>
+  );
+}
+
+export default App;
